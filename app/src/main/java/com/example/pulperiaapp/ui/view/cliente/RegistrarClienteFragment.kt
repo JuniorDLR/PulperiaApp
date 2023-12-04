38fa6cc18a645dacb40832e655cc20f1b8ebe465
JuniorDLR <juniordelarocha15@hotmail.com>
2023-12-04 10:59:24 -0600

    Verificacion si un cliente esta registrado

--- app/src/main/java/com/example/pulperiaapp/ui/view/cliente/RegistrarClienteFragment.kt
+++ app/src/main/java/com/example/pulperiaapp/ui/view/cliente/RegistrarClienteFragment.kt
@@ -14,6 +14,7 @@ import com.example.pulperiaapp.databinding.FragmentRegistrarClienteBinding
 import com.example.pulperiaapp.ui.view.credito.viewmodel.ClienteViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 @AndroidEntryPoint
 class RegistrarClienteFragment : Fragment() {
@@ -36,30 +37,46 @@ class RegistrarClienteFragment : Fragment() {
 
         binding.btnAgregarMoroso.setOnClickListener { guardarCliente() }
     }
-
     private fun guardarCliente() {
-        val cliente = binding.tvCliente.text.toString()
+        lifecycleScope.launch {
+            val clienteView = binding.tvCliente.text.toString().trim()
+            val lista = clienteViewModel.obtenerAmoros()
 
-        if (cliente.isEmpty()) {
-            AlertDialog.Builder(requireContext())
-                .setTitle("ADVERTENCIA")
-                .setMessage("Debe de ingresar un nombre para continuar")
-                .setPositiveButton("Aceptar") { dialog, _ ->
-                    dialog.dismiss()
-                }.show()
+            if (clienteView.isEmpty()) {
+                AlertDialog.Builder(requireContext())
+                    .setTitle("ADVERTENCIA")
+                    .setMessage("Debe ingresar un nombre para continuar")
+                    .setPositiveButton("Aceptar") { dialog, _ ->
+                        dialog.dismiss()
+                    }.show()
+            } else {
+                val nombreEnMinuscula = clienteView.lowercase(Locale.ROOT)
+                val filtrado = lista.any { it.lowercase(Locale.ROOT) == nombreEnMinuscula }
 
-        } else {
-            lifecycleScope.launch {
-                val entity = ClienteEntity(0, cliente)
-                clienteViewModel.insertarCliente(entity)
-                binding.tvCliente.setText("")
-                Toast.makeText(requireContext(), "Cliente guardado exitosamente", Toast.LENGTH_LONG)
-                    .show()
+                if (filtrado) {
+                    AlertDialog.Builder(requireContext())
+                        .setTitle("ADVERTENCIA")
+                        .setMessage("Este usuario ya existe en la base de datos")
+                        .setPositiveButton("Continuar") { dialog, _ ->
+                            dialog.dismiss()
+                        }.show()
+                } else {
+                    lifecycleScope.launch {
+                        val entity = ClienteEntity(0, clienteView)
+                        clienteViewModel.insertarCliente(entity)
+                        binding.tvCliente.setText("")
+                        Toast.makeText(
+                            requireContext(),
+                            "Cliente guardado exitosamente",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
             }
         }
-
-
     }
 
 
 }
+
+
